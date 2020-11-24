b4573e6ed062d458e7785f7ffbd0967e44c6edb0
isha27255 <isha27255@gmail.com>
2020-11-24 09:55:42 +0530

    first commit

---
+++ weather__app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import WeatherDetails from './Components/WeatherDetails';
+
+function App() {
+  return (
+    <div className="App">
+      <WeatherDetails></WeatherDetails>
+    </div>
+  );
+}
+
+export default App;
---
+++ weather__app/src/Components/WeatherDetails.js
@@ -0,0 +1,62 @@
+import React, { useState} from 'react'
+import axios from 'axios'
+
+const api_key = "7b263c376e730a553a4e7b6efe724e85"
+function WeatherDetails() {
+
+    const [search, setSearch] = useState('')
+    const [weather, setWeather] = useState([])
+
+    const searches = e => {
+        
+        if(e.key === "Enter")
+        {
+            setWeather('')
+            axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${search}&appid=${api_key}`)
+            .then(res => {
+                console.log(res.data)
+                setWeather(res.data)
+            })
+            .catch(err => {
+                console.log(err)
+            })
+        }
+       
+    }
+    return (
+        <div class={(typeof weather.main != "undefined") ? weather.weather[0].main : 'default'}>
+            
+            <input
+                type="text"
+                className="searchbox"
+                placeholder="Search..."
+                onChange={e => setSearch(e.target.value)}
+                onKeyPress={searches}>
+            </input>
+            
+            
+            {(typeof weather.main != "undefined")?
+            (
+                <div>
+                    
+                    <h2 class={weather.weather[0].main === 'Mist' || weather.weather[0].main === 'Rain' ? 'xyz':'abc'}>{weather.name}, {weather.sys.country}</h2>
+                    
+                    <br></br>
+                    
+                   <div class="temp-box">
+                   <p>{ Math.round(weather.main.temp -273)}C</p>
+
+                   </div>
+                   <br></br>
+                   <h1 class={weather.weather[0].main === 'Mist' || weather.weather[0].main === 'Rain' ? 'xyz':'abc'}>{weather.weather[0].main}</h1>
+                   <br></br>
+                </div>
+            ): (
+                <h1 class="def-heading">Weather App - Enter a city to get its weather details</h1>
+            )}
+            
+        </div>
+    )
+}
+
+export default WeatherDetails
